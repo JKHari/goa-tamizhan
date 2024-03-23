2f815cec3b112a984767a9e5c6c5bd3460e98c55
jk_hari <104263832+JKHari@users.noreply.github.com>
2024-03-23 18:24:28 +0530

    feat: seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
-const baseURL = "https://goatamilan.cyclic.app";
+const baseURL = "https://api-goatamilan.cyclic.app";
 
 export default defineNuxtConfig({
   app: {
     head: {
+      title: "Best Goa trip packagesBest Goa to Tamil Nadu Travel Guide: Explore the Best Routes with Goa Tamilan - goatamilan",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Best Goa to Tamil Nadu Travel Guide: Explore the Best Routes with Goa Tamilan - goatamilan",
+        },
+      ],
       script: [
         {
           src: "https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js",
@@ -12,6 +23,7 @@ export default defineNuxtConfig({
         },
       ],
       link: [
+        { rel: "icon", type: "image/x-icon", href: "/title.png" },
         {
           rel: "stylesheet",
           href: "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
@@ -19,7 +31,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  ssr: false,
+  ssr: true,
   devtools: { enabled: false },
   css: [
     "~/assets/css/main.css",
